d80ed900aa5c7b8bba5ab01137d9138b4c48af16
Viktoryia Davydovich <Viktoryia.Davydovich@itechart-group.com>
2019-06-05 12:42:09 +0300

    moved routing

--- src/index.js
+++ src/index.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
-import Tab from '@material-ui/core/Tab';
-import Tabs from '@material-ui/core/Tabs';
-
-import WrapCounterCountainer from './containers/WrapCounterCountainer'
-import AboutContainer from './containers/AboutContainer'
-import NotFoundContainer from './containers/NotFoundContainer'
-
-const routing = (
-    <Router>
-      <div>
-        <Tabs indicatorColor="primary" textColor="primary" value={0}>
-            <Tab label="About us" to='/about' component={Link} value={0}/>
-            <Tab label="Counters" to='/counters' component={Link} value={1}/>
-        </Tabs>
-        <Switch>
-            <Route path="/counters" component={WrapCounterCountainer} />
-            <Route path="/about" component={AboutContainer} />
-            <Route component={NotFoundContainer} />
-        </Switch>
-      </div>
-    </Router>
-)
+import routing from './views/Routing/index'
 
 ReactDOM.render(
     routing,
