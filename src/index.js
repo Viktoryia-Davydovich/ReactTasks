53fbb69be61e9612574d18041075ea06da4f64b7
Viktoryia Davydovich <Viktoryia.Davydovich@itechart-group.com>
2019-06-05 11:54:29 +0300

    added tabs and routes, need to do - active/disabled for tabs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+
 
 import WrapCounterCountainer from './containers/WrapCounterCountainer'
 import AboutContainer from './containers/AboutContainer'
-import MainContainer from './containers/MainContainer'
+
 
 const routing = (
     <Router>
       <div>
-        <Route path="/" component={MainContainer} />
+        <Tabs indicatorColor="primary" textColor="primary" >
+            <Tab label="About us" active to='/about' component={Link}/>
+            <Tab label="Counters" active to='/counters' component={Link}/>
+        </Tabs>
         <Route path="/counters" component={WrapCounterCountainer} />
         <Route path="/about" component={AboutContainer} />
       </div>
