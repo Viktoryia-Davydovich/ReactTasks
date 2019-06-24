53628a1b4c76d082617d97b1440121e5e4e1277c
Viktoryia Davydovich <victoriadavidovich13@gmail.com>
2019-06-24 15:49:34 +0300

    rewrote not found using hooks

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,15 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import Typography from "@material-ui/core/Typography";
 
-class NotFound extends Component {
-  render() {
-
-    return (
+function NotFound() {
+  return (
     <Typography gutterBottom variant="h5" component="h2" align="center">
       404 - Page Not Found
     </Typography>
-    );
-  }
+  );
 }
 
 export default NotFound;
