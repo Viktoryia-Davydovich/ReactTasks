e983c6bd08e1ae8f54ffe91a2132a9627a84a759
Viktoryia Davydovich <victoriadavidovich13@gmail.com>
2019-06-25 11:56:29 +0300

    dark theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { Redirect } from "react-router";
 import { Provider } from "react-redux";
 import jwt_decode from "jwt-decode";
+import Button from "@material-ui/core/Button";
 
 import setAuthToken from "./store/setAuthToken";
 import { setCurrentUser, logoutUser } from "./store/actions/authentication";
@@ -15,6 +16,8 @@ import {
   NotFound
 } from "./pages/";
 import MenuComponent from "./components";
+import styled from "@emotion/styled";
+import { useTheme } from "./components/themeSwitch";
 
 if (localStorage.jwtToken) {
   setAuthToken(localStorage.jwtToken);
@@ -28,21 +31,43 @@ if (localStorage.jwtToken) {
   }
 }
 
+const Wrapper = styled("div")`
+  background: ${props => props.theme.background};
+  width: 100vw;
+  height: 100vh;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen";
+  h1 {
+    color: ${props => props.theme.body};
+  }
+`;
+
 function App() {
+  const themeState = useTheme();
   return (
     <Provider store={store}>
-      <Router>
-        <MenuComponent />
-        <Switch>
-          <Route exact path="/" />
-          <Route path="/counters" component={WrapCounter} />
-          <Route path="/about" component={About} />
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={LoginReduxForm} />
-          <Route path="/404" component={NotFound} />
-          <Route component={() => <Redirect from="*" to="/404" />} />
-        </Switch>
-      </Router>
+      <Wrapper>
+        <Router>
+          <div>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => themeState.toggle()}
+            >
+              {themeState.dark ? "Switch to Light Mode" : "Switch to Dark Mode"}
+            </Button>
+          </div>
+          <MenuComponent />
+          <Switch>
+            <Route exact path="/" />
+            <Route path="/counters" component={WrapCounter} />
+            <Route path="/about" component={About} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={LoginReduxForm} />
+            <Route path="/404" component={NotFound} />
+            <Route component={() => <Redirect from="*" to="/404" />} />
+          </Switch>
+        </Router>
+      </Wrapper>
     </Provider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "./components/themeSwitch";
 
 import App from "./App";
 
-ReactDOM.render(<App></App>, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
